08e802af83c02e8779839b0d325b8afe3e5fe34d
doman89 <mateusz.domanski.1989@gmail.com>
2018-10-26 22:39:33 +0200

    update all script, and fix view on safari

--- js/script.js
+++ js/script.js
@@ -1,43 +1,45 @@
-const menuIcon = document.querySelector(`.hamburger`);
-const icons = document.querySelectorAll(`.fas`);
-const menu = document.querySelector(`aside`);
-const link = document.querySelectorAll(`li`);
-const portfolioButton = document.querySelector(`.button`);
-const modalCloseButton = document.querySelector(`.close`);
-const wrapper = document.querySelector(`.wrapper`);
-const popupWindow = document.querySelector(`.modal`);
+const menuIcon = document.querySelector('.hamburger');
+const icons = document.querySelectorAll('.fas');
+const menu = document.querySelector('aside');
+const link = document.querySelectorAll('li');
+const portfolioButton = document.querySelector('.button');
+const modalCloseButton = document.querySelector('.close');
+const wrapper = document.querySelector('.wrapper');
+const popupWindow = document.querySelector('.modal');
 const speedScrollWebPage = 30;
-const progressBar = document.querySelector(`.progressBar`);
-const header = document.querySelector(`header`);
-const headerImage = document.querySelector(`header img`);
-const arrowNavigation = document.querySelector(`.arrowNavigation`);
-const upArrow = document.querySelector(`.upArrow`);
-const downArrow = document.querySelector(`.downArrow`);
+const progressBar = document.querySelector('.progressBar');
+const header = document.querySelector('header');
+const headerImage = document.querySelector('header img');
+const arrowNavigation = document.querySelector('.arrowNavigation');
+const upArrow = document.querySelector('.upArrow');
+const downArrow = document.querySelector('.downArrow');
 let timerScrollWebPage;
+const landscape = window.innerWidth > window.innerHeight;
 
 //tablica położenia sekcji na stronie
 let documentElementsPosition = [
-    document.querySelector(`header`).offsetTop,
-    document.querySelector(`.aboutme`).offsetTop,
-    document.querySelector(`.technology`).offsetTop,
-    document.querySelector(`.projects`).offsetTop,
-    document.querySelector(`.motto`).offsetTop,
-    document.querySelector(`.contact`).offsetTop
+    document.querySelector('header').offsetTop,
+    document.querySelector('.aboutme').offsetTop,
+    document.querySelector('.technology').offsetTop,
+    document.querySelector('.projects').offsetTop,
+    document.querySelector('.motto').offsetTop,
+    document.querySelector('.contact').offsetTop
 ];
 
 const documentElementsPositionUpdate = () => {
     documentElementsPosition = [
-        document.querySelector(`header`).offsetTop,
-        document.querySelector(`.aboutme`).offsetTop,
-        document.querySelector(`.technology`).offsetTop,
-        document.querySelector(`.projects`).offsetTop,
-        document.querySelector(`.motto`).offsetTop,
-        document.querySelector(`.contact`).offsetTop
+        document.querySelector('header').offsetTop,
+        document.querySelector('.aboutme').offsetTop,
+        document.querySelector('.technology').offsetTop,
+        document.querySelector('.projects').offsetTop,
+        document.querySelector('.motto').offsetTop,
+        document.querySelector('.contact').offsetTop
     ]
 }
 
 //aktualizacja pozycji sekcji po zmianie rozmiaru okna - bez tego sypie sie :)
-window.addEventListener(`resize`, documentElementsPositionUpdate);
+window.addEventListener('resize', documentElementsPositionUpdate);
+window.addEventListener('scroll', documentElementsPositionUpdate);
 //typ wyliczeniowy do pracy z sekcjami na stronie
 const enumPosition = {
     HEADER: 0,
@@ -50,21 +52,15 @@ const enumPosition = {
 
 //obsługa kliknięcia hamburgera
 const toggleMenuHamburger = () => {
-    menuIcon.classList.toggle(`active`);
+    menuIcon.classList.toggle('active');
     for (let i = 0; i < icons.length; i++)
-        icons[i].classList.toggle(`active`);
-    menu.classList.toggle(`active`);
-    if (window.innerHeight > window.innerWidth) {
-        header.classList.toggle(`active`);
-        wrapper.classList.toggle(`active`);
-        arrowNavigation.classList.toggle(`active`);
+        icons[i].classList.toggle('active');
+    menu.classList.toggle('active');
+    if (!landscape) {
+        header.classList.toggle('active');
+        wrapper.classList.toggle('active');
+        arrowNavigation.classList.toggle('active');
     }
-}
-
-//obsługa zamknięcia popupa
-const closePopupWindow = () => {
-    wrapper.classList.add(`active`);
-    popupWindow.classList.remove(`active`);
 }
 
 //scrollowanie do określonej pozycji strony
@@ -89,13 +85,13 @@ const scrollWebPage = (positionY) => {
 }
 
 //nasłuchiwanie na ikonke hamburgera oraz na hiperłącza
-portfolioButton.addEventListener(`click`, () => {
+portfolioButton.addEventListener('click', () => {
     timerScrollWebPage = setTimeout(`scrollWebPage(${documentElementsPosition[enumPosition.PROJECTS]})`, 1);
 });
 // modalCloseButton.addEventListener("click", closePopupWindow);
-menuIcon.addEventListener(`click`, toggleMenuHamburger);
+menuIcon.addEventListener('click', toggleMenuHamburger);
 for (let i = 0; i < link.length; i++) {
-    link[i].addEventListener(`click`, () => {
+    link[i].addEventListener('click', () => {
         toggleMenuHamburger();
         timerScrollWebPage = setTimeout(`scrollWebPage(${documentElementsPosition[i]})`, 1);
     });
@@ -106,86 +102,102 @@ for (let i = 0; i < link.length; i++) {
 const scrollEffectinSection = (typeSection, childNumber, onOff = true) => {
     if (onOff === true) {
         for (let i = 1; i <= childNumber; i++) {
-            document.querySelector(`${typeSection}:nth-of-type(${i})`).classList.add(`active`);
+            document.querySelector(`${typeSection}:nth-of-type(${i})`).classList.add('active');
         }
     } else {
         for (let i = 1; i <= childNumber; i++) {
-            document.querySelector(`${typeSection}:nth-of-type(${i})`).classList.remove(`active`);
+            document.querySelector(`${typeSection}:nth-of-type(${i})`).classList.remove('active');
         }
     }
 }
 
 
 //nasłuchiwanie na scrolowanie strony i efekty do tego
-window.addEventListener(`scroll`, () => {
+window.addEventListener('scroll', () => {
     const currentPercent = Math.round(window.pageYOffset / (wrapper.offsetHeight - window.innerHeight) * 100);
-    const userPosition = window.scrollY + window.innerHeight;
-    progressBar.style.width = currentPercent + `%`;
-    if (window.scrollY < documentElementsPosition[enumPosition.ABOUTME]) {
-        header.style.backgroundColor = `rgba(21, 162, 255, ${1-(1.5 * window.scrollY / documentElementsPosition[enumPosition.ABOUTME])})`;
-        headerImage.style.opacity = 1 - (1.5 * window.scrollY / documentElementsPosition[enumPosition.ABOUTME]);
-    }
-    if ((userPosition > documentElementsPosition[enumPosition.ABOUTME]) && (window.scrollY < documentElementsPosition[enumPosition.TECHNOLOGY])) {
-        const userPositionInSection = ((userPosition - documentElementsPosition[enumPosition.ABOUTME]) / (documentElementsPosition[enumPosition.TECHNOLOGY] - documentElementsPosition[enumPosition.ABOUTME]));
-        if (userPositionInSection > 0.8) {
-            scrollEffectinSection(`.aboutme p`, 2);
-        } else if (userPositionInSection > 0.4) {
-            scrollEffectinSection(`.aboutme p`, 1);
+    const bottomWindow = window.scrollY + window.innerHeight;
+    const topSectionAbout = documentElementsPosition[enumPosition.ABOUTME];
+    const topSectionTechnology = documentElementsPosition[enumPosition.TECHNOLOGY];
+    const topSectionProjects = documentElementsPosition[enumPosition.PROJECTS];
+    const topSectionMotto = documentElementsPosition[enumPosition.MOTTO];
+
+    //progressBar
+    progressBar.style.width = `${currentPercent}%`;
+
+    //efekt znikania header + display none (safari błędnie wyswietla strone bez tegos)
+    if (window.scrollY < topSectionAbout) {
+        header.style.backgroundColor = `rgba(21, 162, 255, ${1-(1.5 * window.scrollY / topSectionAbout)})`;
+        headerImage.style.opacity = 1 - (1.5 * window.scrollY / topSectionAbout);
+        header.style.display = 'flex';
+    } else
+        header.style.display = 'none';
+
+    //efekty pojawiania się sekcji aboutme
+    if ((bottomWindow > topSectionAbout) && (window.scrollY < topSectionTechnology)) {
+        const bottomWindowInSection = ((bottomWindow - topSectionAbout) / (topSectionTechnology - topSectionAbout));
+        if (bottomWindowInSection > 0.8) {
+            scrollEffectinSection('.aboutme p', 2);
+        } else if (bottomWindowInSection > 0.4) {
+            scrollEffectinSection('.aboutme p', 1);
         }
     } else {
-        scrollEffectinSection(`.aboutme p`, 2, false);
+        scrollEffectinSection('.aboutme p', 2, false);
     }
-    if ((userPosition > documentElementsPosition[enumPosition.TECHNOLOGY]) && (window.scrollY < documentElementsPosition[enumPosition.PROJECTS])) {
-        const userPositionInSection = ((userPosition - documentElementsPosition[enumPosition.TECHNOLOGY]) / (documentElementsPosition[enumPosition.PROJECTS] - documentElementsPosition[enumPosition.TECHNOLOGY]));
-        if (window.innerWidth > window.innerHeight) {
-            if (userPositionInSection > 0.75) {
-                scrollEffectinSection(`.technology .language`, 5);
-            } else if (userPositionInSection > 0.20) {
-                scrollEffectinSection(`.technology .language`, 3);
+
+    //efekty pojawiania się sekcji technology
+    if ((bottomWindow > topSectionTechnology) && (window.scrollY < topSectionProjects)) {
+        const bottomWindowInSection = ((bottomWindow - topSectionTechnology) / (topSectionProjects - topSectionTechnology));
+        if (landscape) {
+            if (bottomWindowInSection > 0.75) {
+                scrollEffectinSection('.technology .language', 5);
+            } else if (bottomWindowInSection > 0.20) {
+                scrollEffectinSection('.technology .language', 3);
             }
         } else {
-            if (userPositionInSection > 0.85) {
-                scrollEffectinSection(`.technology .language`, 5);
-            } else if (userPositionInSection > 0.71) {
-                scrollEffectinSection(`.technology .language`, 4);
-            } else if (userPositionInSection > 0.50) {
-                scrollEffectinSection(`.technology .language`, 3);
-            } else if (userPositionInSection > 0.30) {
-                scrollEffectinSection(`.technology .language`, 2);
-            } else if (userPositionInSection > 0.05) {
-                scrollEffectinSection(`.technology .language`, 1);
+            if (bottomWindowInSection > 0.85) {
+                scrollEffectinSection('.technology .language', 5);
+            } else if (bottomWindowInSection > 0.71) {
+                scrollEffectinSection('.technology .language', 4);
+            } else if (bottomWindowInSection > 0.50) {
+                scrollEffectinSection('.technology .language', 3);
+            } else if (bottomWindowInSection > 0.30) {
+                scrollEffectinSection('.technology .language', 2);
+            } else if (bottomWindowInSection > 0.05) {
+                scrollEffectinSection('.technology .language', 1);
             }
         }
     } else {
-        scrollEffectinSection(`.technology .language`, 5, false);
+        scrollEffectinSection('.technology .language', 5, false);
     }
-    if ((userPosition > documentElementsPosition[enumPosition.PROJECTS]) && (window.scrollY < documentElementsPosition[enumPosition.MOTTO])) {
-        const userPositionInSection = ((userPosition - documentElementsPosition[enumPosition.PROJECTS]) / (documentElementsPosition[enumPosition.MOTTO] - documentElementsPosition[enumPosition.PROJECTS]));
-        if (window.innerWidth > window.innerHeight) {
-            if (userPositionInSection > 0.90) {
-                scrollEffectinSection(`.project`, 3);
-            } else if (userPositionInSection > 0.60) {
-                scrollEffectinSection(`.project`, 2);
-            } else if (userPositionInSection > 0.25) {
-                scrollEffectinSection(`.project`, 1);
+
+    //efekty animacji w sekcji projects
+    if ((bottomWindow > topSectionProjects) && (window.scrollY < topSectionMotto)) {
+        const bottomWindowInSection = ((bottomWindow - topSectionProjects) / (topSectionMotto - topSectionProjects));
+        if (landscape) {
+            if (bottomWindowInSection > 0.85) {
+                scrollEffectinSection('.project', 3);
+            } else if (bottomWindowInSection > 0.60) {
+                scrollEffectinSection('.project', 2);
+            } else if (bottomWindowInSection > 0.20) {
+                scrollEffectinSection('.project', 1);
             }
         } else {
-            if (userPositionInSection > 0.80) {
-                scrollEffectinSection(`.project`, 3);
-            } else if (userPositionInSection > 0.50) {
-                scrollEffectinSection(`.project`, 2);
-            } else if (userPositionInSection > 0.20) {
-                scrollEffectinSection(`.project`, 1);
+            if (bottomWindowInSection > 0.85) {
+                scrollEffectinSection('.project', 3);
+            } else if (bottomWindowInSection > 0.45) {
+                scrollEffectinSection('.project', 2);
+            } else if (bottomWindowInSection > 0.20) {
+                scrollEffectinSection('.project', 1);
             }
         }
     } else {
-        scrollEffectinSection(`.project`, 3, false);
+        scrollEffectinSection('.project', 3, false);
     }
-    documentElementsPositionUpdate();
+    // documentElementsPositionUpdate();
 })
 
 //funkcja obsługi klawiszy w lewo i prawo
-const scrollSection = (e) => {
+const scrollSection = e => {
     if (((e.keyCode == 38) || (e == 38)) && (timerScrollWebPage == null)) {
         for (let iteration = 0; iteration < documentElementsPosition.length; iteration++) {
             if ((iteration == (documentElementsPosition.length - 1)) && (documentElementsPosition[iteration] <= window.pageYOffset)) {
@@ -208,11 +220,11 @@ const scrollSection = (e) => {
 }
 
 //nasłuchiwanie klawiszy góra/dół oraz strzałek góra/dół na stronie
-window.addEventListener(`keydown`, scrollSection);
+window.addEventListener('keydown', scrollSection);
 
-upArrow.addEventListener(`click`, () => {
+upArrow.addEventListener('click', () => {
     scrollSection(38);
 });
-downArrow.addEventListener(`click`, () => {
+downArrow.addEventListener('click', () => {
     scrollSection(40);
 });
